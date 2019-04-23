625491d4772b0a4fde0f920c65b6693c2ce9778f
LG-17864078090 <13954027535@163.com>
2019-04-23 21:40:51 +0800

    2019.04.23

---
+++ 5_school/application/views/footer.php
@@ -0,0 +1,12 @@
+<div class="footer">© 学生学情管理系统</div>
+<style>
+    .footer {
+        margin-top:20px;
+        padding-bottom:50px;
+        height:38px;
+        line-height:38px;
+        border-top:2px solid #CCCCCC;
+        color:#737573;
+        text-align:center;
+    }
+</style>
